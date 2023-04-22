8a7c635422c5bc906cd4c5b4f15fd9831a6332bc
shin.jewoo <shinjw0926@naver.com>
2023-04-22 22:14:44 +0900

    쿼리 메소드 기능 - @EntityGraph

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -221,4 +221,41 @@ class MemberRepositoryTest {
 
         assertThat(result).isEqualTo(3);
     }
+
+    @Test
+    void findMemberLazy() throws Exception {
+        //given
+        //member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+//        List<Member> members = memberRepository.findMemberFetchJoin();
+        List<Member> members = memberRepository.findAll();
+        List<Member> memberList = memberRepository.findEntityGraphByUsername("member1");
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+
+        for (Member member : memberList) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+
+        //then
+    }
 }
